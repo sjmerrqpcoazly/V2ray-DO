a271c5fd39cae3b93080c88bace1da35e3252656
Hora Pusa <73831309+Horapusaa@users.noreply.github.com>
2022-01-15 23:27:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #/user/bin/env/pythoon
 
 import subprocess
-subprocess.run("bash xray.sh",shell=True)   
-
+subprocess.run("sudo chmod 777 main.sh",shell=True)    
+subprocess.run("sudo bash main.sh",shell=True)   
 exit()
